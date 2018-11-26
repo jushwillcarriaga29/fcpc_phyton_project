0738ccc3d0277908b65b4794888027f1e1f843bd
jushwillcarriaga29 <45374703+jushwillcarriaga29@users.noreply.github.com>
2018-11-27 07:51:57 +0800

    Create declaration_carriaga.py

---
+++ declaration_carriaga.py
@@ -0,0 +1,28 @@
+#Numerical Chuchu
+
+Sum = " 1 + 100 "
+Diff = " 100 - 12 "
+Product = "10 * 5 "
+Quotient = " 10 / 2 "
+
+print (Sum)
+print (Diff)
+print (Product)
+print (Quotient)
+
+#Arithnetic
+
+division = 100 / 10
+subtraction = 90 - 70
+modulus = 100 % 10
+
+print("division")
+print("subtraction")
+print("modulus")
+
+if division > subtraction:
+    print("kaya to")
+elif subtraction > modulus:
+    print("basic lang to")
+else:
+    print("kaya mo to jush")
