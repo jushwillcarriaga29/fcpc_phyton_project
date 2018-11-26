35e2190297f59c8d04ee88769e17b74bff9c9d68
jushwillcarriaga29 <45374703+jushwillcarriaga29@users.noreply.github.com>
2018-11-27 07:55:59 +0800

    Create dynamic input_carriaga.py

---
+++ dynamic input_carriaga.py
@@ -0,0 +1,9 @@
+#accepts input from user
+name = input (&quot;Enter your name:&quot;)
+print(&quot;You&#39;re name is:&quot; + name)
+#accepts numerical input
+fnum = int(input(&quot;enter first number:&quot;))
+snum = int (input(&quot;enter second number&quot;))
+to_int_value=int(&quot;3&quot;)
+print(fnum+snum)
+print(to_int_value ** 3)
