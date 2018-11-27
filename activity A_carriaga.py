bde50ebeb5f0439abc68804d1f9c63d6541a5eac
jushwillcarriaga29 <45374703+jushwillcarriaga29@users.noreply.github.com>
2018-11-27 11:16:46 +0800

    Create activity A_carriaga.py

---
+++ activity A_carriaga.py
@@ -0,0 +1,28 @@
+#Activity A
+
+
+#1st
+print("una")
+for x in range (1,17,3):
+
+    print(x, end='\t')
+print()
+print()
+#2nd
+print("pangalawa")
+for x in range (100,75,-7):
+
+    print(x,end='\t')
+print()
+print()
+#3rd
+
+print("pangatlo")
+for x in range(1,5):
+
+    if x % 3 == 0:
+       print ("1", end = '\t')
+    else:
+       print("0", end = '\t')
+print()
+print()
