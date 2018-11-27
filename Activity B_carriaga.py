27c7429438bb2f04ada246262f82849f7b835aed
jushwillcarriaga29 <45374703+jushwillcarriaga29@users.noreply.github.com>
2018-11-27 11:28:20 +0800

    Create Activity B_carriaga.py

---
+++ Activity B_carriaga.py
@@ -0,0 +1,27 @@
+Activity B
+
+
+#1st
+print("una")
+for x in range(8):
+    print(2**x, end = '\t')
+print()
+print()
+
+#2nd
+print("pangalawa")
+for x in range(1,13):
+    if x % 4 == 0:
+        print("S", end = '\t')
+    else:
+        print(x, end = '\t')
+print()
+print()
+
+#3rd
+print("pangatlo")
+for x in range(1, 9):
+    if x % 8 == 1:
+        print(x, end = '\t')
+    else:
+        print("E", end = '\t')
